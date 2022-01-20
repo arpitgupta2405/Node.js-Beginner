0ab7e6df4e43f896c9c2d348363ac8b7d352cacb
yashmodi2911 <yashmodi817@gmail.com>
2022-01-20 21:28:17 +0530

    Correcting mistake for 2nd question

--- js_assignment2.js
+++ js_assignment2.js
@@ -1,20 +1,31 @@
 function MetadataParser(version,channel,keyfield){
-   this._channel=this.setChannel(channel);
-   this._version=this.setVersion(version);
-   this._keyfield=this.setKeyField(keyfield);
+   this._channel=0;
+   this._version="";
+   this._keyfield="";
+   this.setVersion(version);
+   this.setChannel(channel);
+   this.setKeyField(keyfield);
 }  
-MetadataParser.prototype.setVersion = (version) =>_version=version;
-MetadataParser.prototype.setChannel = (channel) => _channel=channel;
-MetadataParser.prototype.setKeyField = (keyfield) => _keyfield=keyfield;
+MetadataParser.prototype.setVersion = function(version){
+    this._version=version;
+}
+MetadataParser.prototype.setChannel = function(channel){
+    this._channel=channel;
+}
+MetadataParser.prototype.setKeyField = function(keyfield){
+    this._keyfield=keyfield;
+}
 
-MetadataParser.prototype.getVersion = ()=>{ 
-    return _version;
+MetadataParser.prototype.getVersion = function(){ 
+    return this._version;
 };
-MetadataParser.prototype.getChannel = ()=>{ 
-    return _channel;
+MetadataParser.prototype.getChannel = function(){ 
+    return this._channel;
 };
-MetadataParser.prototype.getKeyField = ()=>{ 
-    return _keyfield;
+MetadataParser.prototype.getKeyField = function(){ 
+    return this._keyfield;
 };
 const person = new MetadataParser('1.0.0',2,'engineer');
 console.log(person.getVersion());
+console.log(person.getChannel());
+console.log(person.getKeyField());
